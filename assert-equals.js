400c9327b2d7825423de0f82abfbb43ddf0c72f2
raphaella-rose <raphaella.rose1@gmail.com>
2022-09-06 13:07:19 +0100

    refactored assertEquals to throw dynamic error when passed two different types

--- assert-equals.js
+++ assert-equals.js
@@ -4,12 +4,8 @@ const assertEquals = (expect, actual) => {
         throw `Expected "${expect}" but found "${actual}"`
     } else if (typeof expect == "number" && expect != actual) {
         throw `Expected ${expect} but found ${actual}`
-    } else if (typeof expect != typeof actual && expect == 1) {
-        throw `Expected type number but found type string`;
-    } else if (typeof expect != typeof actual && expect == '2') {
-        throw `Expected type string but found type number`;
-    } else if (typeof expect != typeof actual && expect == 4) {
-        throw `Expected type number but found type string`;
+    } else if (typeof expect != typeof actual) {
+        throw `Expected type ${typeof expect} but found type ${typeof actual}`;
     } 
 
 
