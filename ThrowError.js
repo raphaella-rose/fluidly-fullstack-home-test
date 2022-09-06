a16eb5c092da8f66e1e5248d85bbcd5acf8fa4bd
raphaella-rose <raphaella.rose1@gmail.com>
2022-09-06 15:10:20 +0100

    refactored ThrowError string function

--- ThrowError.js
+++ ThrowError.js
@@ -1,13 +1,6 @@
 class ThrowError {
     string(expect, actual) {
-        if (expect == "abcef") {
-            return 'Expected "abcef" but found "abc"'
-        } else if (expect == "dog") {
-            return 'Expected "dog" but found "dogs"'
-        } else {
-            return 'Expected "chocolate" but found "sweets"'
-        }
-       
+        return `Expected "${expect}" but found "${actual}"`
     }
 }
 
