15e1cec7fcc25bf71ec88a7dadb6551332c28c58
raphaella-rose <raphaella.rose1@gmail.com>
2022-09-06 13:09:44 +0100

    refactored checkType out of assertEquals function

--- assert-equals.js
+++ assert-equals.js
@@ -1,11 +1,16 @@
+const checkType = (expect, actual) => {
+    if (typeof expect != typeof actual) {
+        throw `Expected type ${typeof expect} but found type ${typeof actual}`;
+    }
+}
 
 const assertEquals = (expect, actual) => {
+    checkType(expect, actual)
+    
     if (typeof expect == "string" && expect != actual) {
         throw `Expected "${expect}" but found "${actual}"`
     } else if (typeof expect == "number" && expect != actual) {
         throw `Expected ${expect} but found ${actual}`
-    } else if (typeof expect != typeof actual) {
-        throw `Expected type ${typeof expect} but found type ${typeof actual}`;
     } 
 
 
