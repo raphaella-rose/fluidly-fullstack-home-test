09eacc88bb6ac02713b7dd548aad7c2cde0c4d66
raphaella-rose <raphaella.rose1@gmail.com>
2022-09-06 21:32:56 +0100

    unit test for two types using AssertEquals class

--- AssertEquals.spec.js
+++ AssertEquals.spec.js
@@ -15,7 +15,7 @@ describe(AssertEquals, () => {
     // testing for 2 strings
     describe('when expected and actual are different strings, "abcef" "abc"', () => {
         it('throws an error', () => {
-            const throwErrorDouble = { string: () => ('Expected "abcef" but found "abc"') }
+            const throwErrorDouble = { string: () => ('Expected "abcef" but found "abc"') };
             const assertEquals = new AssertEquals('abcef', 'abc', throwErrorDouble);
             expect(() => assertEquals.isEqual()).toThrow('Expected "abcef" but found "abc"');
     })
@@ -24,9 +24,18 @@ describe(AssertEquals, () => {
    // testing for two numbers
    describe('when expected and actual are different numbers, 1 2', () => {
         it('throws an error', () => {
-            const throwErrorDouble = { number: () => ('Expected 1 but found 2') }
+            const throwErrorDouble = { number: () => ('Expected 1 but found 2') };
             const assertEquals = new AssertEquals(1, 2, throwErrorDouble);
             expect(() => assertEquals.isEqual()).toThrow('Expected 1 but found 2');
     })
   })
+
+    // testing for a number and a string
+    describe('when expected and actual are different types, 1 "1"', () => {
+        it('throws an error', () => {
+            const throwErrorDouble = { type: () => ('Expected type number but found type string') }
+            const assertEquals = new AssertEquals(1, '1', throwErrorDouble);
+            expect(() => assertEquals.isEqual()).toThrow('Expected type number but found type string');
+        })
+      })
 })
