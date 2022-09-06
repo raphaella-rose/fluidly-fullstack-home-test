619293f26eb313a842a324a4d41986b99abf5b99
raphaella-rose <raphaella.rose1@gmail.com>
2022-09-06 21:29:55 +0100

    unit test for two strings using AssertEquals class

--- AssertEquals.spec.js
+++ AssertEquals.spec.js
@@ -4,12 +4,20 @@ describe(AssertEquals, () => {
 
     describe('when expected and actual are the same string or number', () => {
         it('returns without throwing an error', () => {
-      
-            const throwErrorDouble = { isEqual: () => (null) }
+            const throwErrorDouble = { string: () => (null) };
             const assertEqualsString = new AssertEquals('abc', 'abc', throwErrorDouble);
             const assertEqualsNumber = new AssertEquals(1, 1, throwErrorDouble);
             expect(() => assertEqualsString.isEqual().not.toThrow())
             expect(() => assertEqualsNumber.isEqual().not.toThrow())
         })
       })
+
+    // testing for 2 strings
+    describe('when expected and actual are different strings, "abcef" "abc"', () => {
+        it('throws an error', () => {
+            const throwErrorDouble = { string: () => ('Expected "abcef" but found "abc"') }
+            const assertEquals = new AssertEquals('abcef', 'abc', throwErrorDouble);
+            expect(() => assertEquals.isEqual()).toThrow('Expected "abcef" but found "abc"');
+    })
+  })
 })
