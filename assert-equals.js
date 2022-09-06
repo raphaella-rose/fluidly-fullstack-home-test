f8a9d6c7a4c5d7c65faa029fc5754db07fd83de7
raphaella-rose <raphaella.rose1@gmail.com>
2022-09-06 14:35:36 +0100

    refactored throwError out of assertEquals

--- assert-equals.js
+++ assert-equals.js
@@ -1,16 +1,24 @@
+const assertEquals = (expect, actual) => {
+    checkType(expect, actual)
+    if (expect != actual) {
+        throwError(expect, actual)
+    } 
+}
+
 const checkType = (expect, actual) => {
     if (typeof expect != typeof actual) {
         throw `Expected type ${typeof expect} but found type ${typeof actual}`;
     }
-    return typeof expect;
 }
 
-const assertEquals = (expect, actual) => {
-    if (checkType(expect, actual) == "string" && expect != actual) {
+const throwError = (expect, actual) => {
+    if (typeof expect == "string") {
         throw `Expected "${expect}" but found "${actual}"`
-    } else if (checkType(expect, actual) == "number" && expect != actual) {
+    } else if (typeof expect == "number") {
         throw `Expected ${expect} but found ${actual}`
     } 
 }
+
+
 
 module.exports = assertEquals
