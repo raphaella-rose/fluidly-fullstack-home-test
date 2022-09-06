fd714914321bb523c60c5cd05e73811318c86b6e
raphaella-rose <raphaella.rose1@gmail.com>
2022-09-06 12:45:57 +0100

    refactored assertEquals to throw dynamic error when two numbers are not the same

--- assert-equals.js
+++ assert-equals.js
@@ -2,13 +2,9 @@ const assertEquals = (expect, actual) => {
     if (expect != actual) {
         if (typeof expect == "string") {
             throw `Expected "${expect}" but found "${actual}"`
-        } else if (expect == 1) {
-            throw `Expected 1 but found 2`
-        } else if (expect == 3) {
-            throw `Expected 3 but found 5`
         } else {
-            throw `Expected 2 but found 7`
-        }
+            throw `Expected ${expect} but found ${actual}`
+        } 
         
     }
     
