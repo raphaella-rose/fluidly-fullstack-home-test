ec7cec8d8b50f7b636a9f58c7c2cc3841a359a02
raphaella-rose <raphaella.rose1@gmail.com>
2022-09-06 12:33:44 +0100

    refactored function to return error when expected and actual are different strings

--- assert-equals.js
+++ assert-equals.js
@@ -1,11 +1,8 @@
 const assertEquals = (expect, actual) => {
-    if (expect != actual && expect == "abcef") {
-        throw 'Expected "abcef" but found "abc"'
-    } else if (expect != actual && expect == "dog") {
-        throw 'Expected "dog" but found "dogs"'
-    } else if (expect != actual && expect == "chocolate") {
-        throw 'Expected "chocolate" but found "sweets"'
+    if (expect != actual) {
+        throw `Expected "${expect}" but found "${actual}"`
     }
+  
     
 }
 
